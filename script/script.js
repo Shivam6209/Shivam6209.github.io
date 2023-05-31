c2c46d783509ae41bfc054f8b36d577de72e90fe
Shivam6209 <shivammasaistudent@gmail.com>
2023-06-01 01:35:27 +0530

    proto

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Java Backend Developer","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
